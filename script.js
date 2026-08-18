1fe32121097a967600de6add7f2d07caf9ca64de
ryuka <domodachiiwa@gmail.com>
2026-08-18 17:16:15 -0300

    script.js

--- script.js
+++ script.js
@@ -3,25 +3,37 @@ const acceptBtn = document.getElementById("acceptBtn");
 const declineBtn = document.getElementById("declineBtn");
 
 function changeMessage(content) {
+
   message.classList.remove("changed");
 
-  // Restart animation
+  // Restart the CSS animation
   void message.offsetWidth;
 
   message.innerHTML = content;
+
   message.classList.add("changed");
 }
 
+
+/* ACCEPT */
+
 acceptBtn.addEventListener("click", () => {
+
   changeMessage(`
     <p>( ♥‿♥ ) &nbsp; friend request accepted!</p>
     <p>(100%) friendship unlocked ♡</p>
   `);
+
 });
 
+
+/* DECLINE */
+
 declineBtn.addEventListener("click", () => {
+
   changeMessage(`
     <p>(╥﹏╥) &nbsp; request declined...</p>
     <p>maybe next time? ♡</p>
   `);
+
 });
